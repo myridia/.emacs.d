172e87ceaffc95969be0f82b645268cc4b410117
veto <veto@body.body-piercing.com>
2026-03-26 08:44:50 +0700

    up: requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
+sudo apt-get install shfmt -y
 sudo apt-get install clang-format -y
-sudo apt-get install clangd php-cli  -y 
-wget https://cs.symfony.com/download/php-cs-fixer-v3.phar -O php-cs-fixer 
-sudo chmod a+x php-cs-fixer 
+sudo apt-get install clangd php-cli -y
+wget https://cs.symfony.com/download/php-cs-fixer-v3.phar -O php-cs-fixer
+sudo chmod a+x php-cs-fixer
 sudo mv php-cs-fixer /usr/bin/php-cs-fixer
-sudo  curl -Lo phpactor.phar https://github.com/phpactor/phpactor/releases/latest/download/phpactor.phar
-sudo chmod +x  phpactor.phar
+sudo curl -Lo phpactor.phar https://github.com/phpactor/phpactor/releases/latest/download/phpactor.phar
+sudo chmod +x phpactor.phar
 sudo mv phpactor.phar /usr/bin/phpactor
 curl -LsSf https://astral.sh/ruff/install.sh | sh
 sudo npm i -g prettier
-
 
 echo "...install rust"
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 echo "...update rust"
 rustup update
-git clone https://github.com/rust-analyzer/rust-analyzer.git 
+git clone https://github.com/rust-analyzer/rust-analyzer.git
 cd rust-analyzer
-cargo xtask install --server 
+cargo xtask install --server
 
-sudo npm i -g prettier 
+sudo npm i -g prettier
